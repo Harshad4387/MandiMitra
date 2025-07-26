2bb25d86f9324ae491a17d127f5f1d943c1717c8
Harshad4387 <harshadjogdande25@gmail.com>
2025-07-26 14:06:20 +0530

    adding profile featue

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -20,6 +20,7 @@ const verifyjwt = async (req,res,next)=>{
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
+    
 
     req.user = user;
 
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const {signup , login , logout ,checkauth ,profile} = require("../controllers/auth.controller.js");
-// const verifyjwt = require("../middlewares/auth.middlewares.js");
+const verifyjwt = require("../middlewares/auth.middlewares.js");
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup",signup);
 router.post("/logout",logout);
-// router.get("/profile",verifyjwt,profile);
+router.get("/profile",verifyjwt,profile);
 // router.get("/check",verifyjwt, checkauth);
 
 
